945b37ed4e019e0e57db1e9ba4197e319b1bc314
PeterBuerer <peterjbuerer@gmail.com>
2016-12-20 20:04:45 -0500

    Images can be dragged and won't go off edges.

---
+++ Joiners/DraggableImageView.swift
@@ -0,0 +1,14 @@
+//
+//  DraggableImageView.swift
+//  Joiners
+//
+//  Created by Peter Buerer on 11/4/16.
+//  Copyright © 2016 Peter Buerer. All rights reserved.
+//
+
+import UIKit
+
+class DraggableImageView: UIImageView {
+    var horizontalConstraint = NSLayoutConstraint()
+    var verticalConstraint = NSLayoutConstraint()
+}
