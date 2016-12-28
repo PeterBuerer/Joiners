606a744e16122d241a3bb136c2d4e5d5d2c7e226
PeterBuerer <peterjbuerer@gmail.com>
2016-12-27 20:14:58 -0500

    Added gitignore

--- Joiners/AppDelegate.swift
+++ Joiners/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = ViewController()
+        let vc = CanvasController()
         let nav = UINavigationController(rootViewController: vc)
         
         window?.rootViewController = nav
